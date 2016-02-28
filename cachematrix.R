c488767f65b241788b881e92861f9654337deb8d
mhowe <mhowe@outlook.com>
2016-02-27 16:34:49 -0800

    Updated functions to not need the extra internal function

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,13 @@
 
 ## Allows caller to set/get a matrix, set/get the inverse value of the matrix 
 ## and check if the inverse is synced to the currently saved matrix value
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(matrixVal = matrix()) {
   mi <- NULL
-  inverseMatchesMatrix <- NULL
-  
-  if( is.na(x) )
-  {
-    matrix(rnorm(1e6),nrow=1e3,ncol=1e3)
-  }
   
   #cache matrix value and indicate that inverse is out of sync with matrix
   set <- function(y) {
     matrixVal <<- y
     mi <<- NULL
-    inverseMatchesMatrix <<- FALSE
   }
   
   #return matrix value
@@ -23,35 +16,30 @@ makeCacheMatrix <- function(x = matrix()) {
   #sets the inverted value of the matrix
   setInversion <- function(inversion) {
     mi <<- inversion
-    inverseMatchesMatrix <<- TRUE
   }
   
   #gets the inverted value of the matrix that is cached
   getInversion <- function() mi
-  #indicates if the matrix and inverted value are in sync
-  getInversionMatchesMatrix <- function() inverseMatchesMatrix
-  
+
   list(set = set, get = get,
        setInversion = setInversion,
-       getInversion = getInversion,
-       getInversionMatchesMatrix = getInversionMatchesMatrix)
+       getInversion = getInversion
+  )
 }
 
 
 ## return the inverse of the passed in matrix
 cacheSolve <- function(x, ...) {
-  invRet <- NULL
-  inverseSet <- x$getInversionMatchesMatrix()
-  
-  #if the matrix has been updated without the inversion being recalculated (or is null), recalculate the inversion
-  if( is.na(inverseSet) || !inverseSet ) {
-    #calculate the inversion and cache it
-    invRet <- solve(x$get)
-    x$setInversion(inv)
-  } else {
-    #get the cached inversion
-    invRet <- x$getInversion  
+  invRet <- x$getInversion()
+
+  if( !is.null(invRet)) {
+    return( invRet )
   }
   
-  return( invRet )
+  #get the matrix, calculate the inverse and cache it
+  matrixVal <- x$get 
+  invRet <- solve( matrixVal ) %*% matrixVal
+  x$setInversion(invRet)
+  
+  invRet
 }
